55185cb6b0c56dfb985eb3a73cabce4123d934c5
Angelo Pirola <angelo9871@gmail.com>
2026-02-19 00:02:03 +0100

    Add ProductCreatedHandler to process ProductCreated events
    Introduced ProductCreatedHandler to handle ProductCreated events via Rebus. The handler creates and saves new OrderProduct entities in the database when a ProductCreated message is received. Added necessary using directives for data access and messaging.

---
+++ src/Microservices.Orders/Handlers/ProductCreatedHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microservices.Orders.DataAccessLayer;
+using Microservices.Orders.DataAccessLayer.Entities;
+using Microservices.Shared.RabbitMQ;
+using Rebus.Handlers;
+
+namespace Microservices.Orders.Handlers;
+
+public class ProductCreatedHandler(ApplicationDbContext dbContext) : IHandleMessages<ProductCreated>
+{
+    public async Task Handle(ProductCreated message)
+    {
+        var newProduct = new OrderProduct
+        {
+            Id = message.Id,
+            Name = message.Name
+        };
+
+        dbContext.OrderProducts.Add(newProduct);
+        await dbContext.SaveChangesAsync();
+    }
+}
