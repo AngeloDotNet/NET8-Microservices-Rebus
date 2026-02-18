75572b135778dc9d54e1b639578eb5ac10bc9e97
Angelo Pirola <angelo9871@gmail.com>
2026-02-19 00:03:16 +0100

    Add DependencyInjection class with DB and RabbitMQ configs
    Introduced a static DependencyInjection class in the Microservices.Shared.Services namespace. This class provides static readonly fields for database and RabbitMQ connection strings, using hardcoded placeholder values for configuration.

---
+++ src/Microservices.Shared/Services/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservices.Shared.Services;
+
+public static class DependencyInjection
+{
+    private static readonly string dbhostname = "YOUR-HOST";
+    private static readonly string dbdatabase = "YOUR-DATABASE";
+    private static readonly string dbusername = "YOUR-USER";
+    private static readonly string dbpassword = "YOUR-PASSWORD";
+
+    public static readonly string database = $"Data Source={dbhostname};Initial Catalog={dbdatabase};User ID={dbusername};Password={dbpassword};Encrypt=False";
+    public static readonly string rabbitConnectionString = "amqp://guest:guest@localhost";
+}
