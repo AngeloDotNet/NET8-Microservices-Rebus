0e1f74f06e6c3bc2d66b5f3e9f7b45d2cc5fe15b
Angelo Pirola <angelo9871@gmail.com>
2026-02-24 16:17:04 +0100

    Add Rebus RabbitMQ producer extension for Products service
    Introduced ServiceCollectionExtensions with AddProducerRabbitMQ,
    configuring Rebus with RabbitMQ transport, type-based routing,
    console logging, System.Text.Json serialization, and a custom
    bus name. Uses shared RabbitMQ connection string.

---
+++ src/Microservices.Products/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using Rebus.Serialization.Json;
+
+namespace Microservices.Products.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddProducerRabbitMQ(this IServiceCollection services)
+    {
+        var rabbitConnection = Shared.Services.DependencyInjection.rabbitConnectionString;
+
+        services.AddRebus(configure => configure
+            .Transport(t => t.UseRabbitMq(rabbitConnection, "microservices-producer"))
+            .Routing(r => r.TypeBased())
+
+            .Options(options => options.SetBusName("ProductsServiceBus"))
+            .Logging(logging => logging.Console(Rebus.Logging.LogLevel.Info))
+            .Serialization(serialize => serialize.UseSystemTextJson())
+        );
+        return services;
+    }
+}
